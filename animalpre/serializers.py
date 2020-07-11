032b27f7b13d808103c868132cd0d4f297df8a39
juniorboycoder <31081190+juniorboycoder@users.noreply.github.com>
2020-07-11 11:07:47 +0100

    Add files via upload

---
+++ animalpre/serializers.py
@@ -0,0 +1,9 @@
+#API generator for animal features model
+from rest_framework import serializers
+from . models import Animalfeatures
+
+class AnimalfeaturesSerializers(serializers.ModelSerializer):
+	class Meta:
+		model=Animalfeatures
+		fields='__all__'
+
